9235027183aee1ee01bb27b6bf8eb972cb127bc0
Elijah <eligenius@gmail.com>
2020-08-25 15:15:40 +0100

    Modify event index page to list upcoming and past evnt categories

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,23 @@
 class Event < ApplicationRecord
-  belongs_to :creator, class_name: 'User', optional: true
-  has_many :event_attendees, foreign_key: :event
-  has_many :attendees, through: :event_attendees, source: :attendee
+  belongs_to :creator, class_name: 'User', optional: true, dependent: :destroy
+  has_many :event_attendees, foreign_key: :event, dependent: :destroy
+  has_many :attendees, through: :event_attendees, source: :attendee, dependent: :destroy
+
+  def self.past
+    events = self.all
+    html = ""
+    events.each do |event|
+      html << "<li>#{event.title}</li>" if event.date < DateTime.now
+    end
+    html.html_safe
+  end
+
+  def self.upcoming
+    events = self.all
+    html = ""
+    events.each do |event|
+      html << "<li>#{event.title}</li>" if event.date > DateTime.now
+    end
+    html.html_safe
+  end
 end
