4b89585cce93e6a4b65945e3ec90b81f003db88b
Elijah <eligenius@gmail.com>
2020-08-26 14:59:35 +0100

    Style index page and user page

--- app/helpers/event_helper.rb
+++ app/helpers/event_helper.rb
@@ -2,10 +2,11 @@ module EventHelper
   def print_events(events)
     html = ""
     events.each do |event|
-      html << "<li>#{event.title}</li>"
+      html << "<p>#{event.title}</p>"
+      html << button_to('Check Event', event_path(event), method: :get)
     end
     html.html_safe
   end
 
-
+  
 end
