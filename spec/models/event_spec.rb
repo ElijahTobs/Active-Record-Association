5a28a3d9181f8e6edadacff9ac2b1d957851c1f9
Elijah <eligenius@gmail.com>
2020-08-28 14:36:12 +0100

    Add association tests for event model

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -19,4 +19,32 @@ RSpec.describe Event, type: :model do
       end
     end
   end
+
+  # ----------------------------------------------------------
+  # ----------------------------------------------------------
+
+  describe 'associations' do 
+    let(:user1){ User.create(name: 'Elijah') }
+    let(:user2){ User.create(name: 'Lucas') }
+    let(:event1){ user1.created_events.create(title: 'The first event') }
+    let(:event2){ user1.created_events.create(title: 'The second event') }
+    
+    context 'User can create more than one event' do
+      # before {user1;event1;event2}
+      it 'is listed in user.created_events' do
+        expect(user1.created_events).to include(event1,event2)
+      end
+    end
+
+     context 'user can attend more than one event' do
+      # before {user;event1;event2}
+      it 'is listed in user.attended_events' do
+        # event.attendees.push(user)
+        user1.attended_events.push(event1,event2)
+        expect(user1.attended_events).to include(event1,event2)
+      end
+    end
+
+    # context 'event can have many attendees' do
+  end
 end
