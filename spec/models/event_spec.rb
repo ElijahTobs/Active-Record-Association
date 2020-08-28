73e326e4b5b4dda18a50ff6c774a00648c0ad278
ldelbel <lucdelbel@gmail.com>
2020-08-28 12:49:04 -0300

    Add associations specs for Event model

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,10 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  let(:event_valid){ Event.create(title: 'This is a valid title')}
-  let(:event_invalid){ Event.create(title: 'Invalid')}
+  let(:user){ User.create(name: 'Elijah') } 
+  let(:user2){ User.create(name: 'Lucas') }
   
   describe 'validation' do 
+    let(:event_valid){ user.created_events.create(title: 'This is a valid title')}
+    let(:event_invalid){ user.created_events.create(title: 'Invalid')}
+  
     context 'event with valid title length' do
       before {event_valid}
       it 'is valid' do
@@ -20,39 +23,55 @@ RSpec.describe Event, type: :model do
     end
   end
 
-  # ----------------------------------------------------------
-  # ----------------------------------------------------------
+  describe 'associations' do
+    let(:event){ user.created_events.create(title: 'Event from user') }
+    
+    context 'user that creates an event' do
+      before {event}
+      it 'is listed in event.creator' do
+        expect(event.creator).to eql(user)
+      end
+    end
 
-  describe 'associations' do 
-    let(:user1){ User.create(name: 'Elijah') }
-    let(:user2){ User.create(name: 'Lucas') }
-    let(:event1){ user1.created_events.create(title: 'The first event') }
-    let(:event2){ user1.created_events.create(title: 'The second event') }
+     context 'user that attends event' do
+      before {event}
+      it 'is listed in event.attendees' do
+        user.attended_events.push(event)
+        expect(event.attendees).to include(user)
+      end
+    end
+  end
+
+
+  describe 'many-to-many relations' do 
+    let(:event11){ user.created_events.create(title: 'The first event from user') }
+    let(:event12){ user.created_events.create(title: 'The second event from user') }
+    let(:event21){ user2.created_events.create(title: 'The first event from user2') }
+    let(:event22){ user2.created_events.create(title: 'The second event from user2') }
     
     context 'User can create more than one event' do
       it 'is listed in user.created_events' do
-        expect(user1.created_events).to include(event1,event2)
+        expect(user.created_events).to include(event11,event12)
       end
     end
 
      context 'user can attend more than one event' do
       it 'is listed in user.attended_events' do
-        user1.attended_events.push(event1,event2)
-        expect(user1.attended_events).to include(event1,event2)
+        user.attended_events.push(event21,event22)
+        expect(user.attended_events).to include(event21,event22)
       end
     end
 
     context 'event can have one or more attendees' do
       it 'allows many attendees' do
-        event1.attendees.push(user1,user2)
-        expect(event1.attendees).to include(user1,user2)
+        event11.attendees.push(user,user2)
+        expect(event11.attendees).to include(user,user2)
       end
 
       it 'can allow one attendee' do
-        event2.attendees.push(user2)
-        expect(event2.attendees).to include(user2)
+        event12.attendees.push(user2)
+        expect(event12.attendees).to include(user2)
       end
-
     end
   end
 end
